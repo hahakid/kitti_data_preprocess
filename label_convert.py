5fba9b61436dba7dadf498f175f6ad32ee398d36
superkid <8140299+hahakid@users.noreply.github.com>
2019-07-16 21:22:20 +0800

    darknet 2 coco label

--- label_convert.py
+++ label_convert.py
@@ -5,7 +5,8 @@ import uuid
 import cv2
 from pprint import pprint
 
-from coco.PythonAPI.pycocotools.coco import COCO
+from cocoapi.PythonAPI.pycocotools.coco import COCO
+import itertools
 
 categories = set()
 
@@ -42,55 +43,65 @@ def parse_label(path, img_id, width, height):
     return annotations
 
 
-dataset_root = '/media/kid/space1/object/training'
-img_root = os.path.join(dataset_root, 'image_2')
-label_root = os.path.join(dataset_root, 'front_view_label')
 
-images = []
-annotations = []
+def process(dataset_type):
+    assert dataset_type in ['tracking', 'object']
 
-img_list = os.listdir(img_root)
-for idx, img_name in enumerate(img_list):
-    img_path = os.path.join(img_root, img_name)
-    height, width = cv2.imread(img_path).shape[:2]
-    img_name_wo_suffix = img_name[:img_name.rfind('.')]
-    label_path = os.path.join(label_root, '{}.txt'.format(img_name_wo_suffix))
+    dataset_root = '/media/kid/space1/{}/training'.format(dataset_type)
+    img_root = os.path.join(dataset_root, 'image_02' if dataset_type == 'tracking' else 'image_2')
+    label_root = os.path.join(dataset_root, 'front_view_label')
 
-    # 解析标签
-    annotation = parse_label(label_path, img_id=img_name, width=width, height=height)
+    images = []
+    annotations = []
+    if dataset_type == 'tracking':
+        img_list = [[os.path.join(seq_root, img_path) for img_path in os.listdir(os.path.join(img_root, seq_root))]
+                    for seq_root in os.listdir(img_root)]
+        img_list = list(itertools.chain(*img_list))
+    elif dataset_type == 'object':
+        img_list = os.listdir(img_root)
 
-    images.append({
-        'license': 3,
-        'file_name': img_name,
-        'height': height,
-        'width': width,
-        'id': img_name_wo_suffix,
-        'coco_url': '', 'date_captured': '', 'flickr_url': '',
-    })
-    annotations.extend(annotation)
+    for idx, img_name in enumerate(img_list):
+        img_path = os.path.join(img_root, img_name)
+        height, width = cv2.imread(img_path).shape[:2]
+        img_name_wo_suffix = img_name[:img_name.rfind('.')]
+        label_path = os.path.join(label_root, '{}.txt'.format(img_name_wo_suffix))
 
-    print('{}/{} {}'.format(idx, len(img_list), img_name))
+        # 解析标签
+        annotation = parse_label(label_path, img_id=img_name, width=width, height=height)
 
-dataset = {
-    'info': {
-        'description': 'KITTI Object',
-        'url': '',
-        'version': '1.0',
-        'year': 2019,
-        'contributor': 'LDMC',
-        'date_created': '2019/07/15'
-    },
-    'images': images,
-    'annotations': annotations,
-    "categories": [{
-        "id": category_id,
-        "name": str(category_id),
-        "supercategory": 'str',
-    } for category_id in categories]
+        images.append({
+            'license': 3,
+            'file_name': img_name,
+            'height': height,
+            'width': width,
+            'id': img_name_wo_suffix,
+            'coco_url': '', 'date_captured': '', 'flickr_url': '',
+        })
+        annotations.extend(annotation)
 
-}
-json.dump(dataset, open('./KITTI_object_front_view.json', 'w'))
-pprint(dataset)
+        #print('{}/{} {}'.format(idx, len(img_list), img_name))
 
-coco_dataset = COCO('./KITTI_object_front_view.json')
-print(len(coco_dataset.dataset))
+    dataset = {
+        'info': {
+            'description': 'KITTI Object',
+            'url': '',
+            'version': '1.0',
+            'year': 2019,
+            'contributor': 'LDMC',
+            'date_created': '2019/07/15'
+        },
+        'images': images,
+        'annotations': annotations,
+        "categories": [{
+            "id": category_id,
+            "name": str(category_id),
+            "supercategory": 'str',
+        } for category_id in categories]
+
+    }
+    json.dump(dataset, open('./KITTI_tracking_front_view.json', 'w'))
+
+#dataset_type = 'object'  #
+#dataset_type = 'tracking'
+process('object')
+process('tracking')
